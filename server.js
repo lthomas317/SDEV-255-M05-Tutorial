bf88f20cc75bff51be8efb822a92c03833789111
lthomas317 <104797969+lthomas317@users.noreply.github.com>
2022-11-27 17:04:40 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer((req, res) => {
+    const num = _.random(0, 20);
+    console.log(num);
+});
+
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+
+    greet();
+    greet();
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+});
